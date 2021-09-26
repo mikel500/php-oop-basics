dfbb11f3fb764df36e17f34f83972c68136d3fa5
mikelmif <mikel@kydemy.com>
2021-09-26 23:59:08 +0200

    Pendiente

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -2,21 +2,5 @@
 
 class ProductController
 {
-    public function index()
-    {
-        echo "<h1> Hello </h1>";
-    }
-
-    public function register()
-    {
-        require_once 'views/users/register.php';
-    }
-    public function postUser()
-    {
-        $register = $_POST["register"];
-
-        if (isset($register) && $register === "register") {
-            var_dump($_POST);
-        }
-    }
+   //pendiente
 }
