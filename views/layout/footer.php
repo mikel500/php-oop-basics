a56043577a896b3472124bea256b9dcc1c431736
mikelmif <mikel@kydemy.com>
2021-09-27 00:12:12 +0200

    Creado footer provisional

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,7 +1,55 @@
 </main>
-<footer class="footer mt-auto p-5 bg-dark text-light">
+<!-- <footer class="footer mt-auto p-5 bg-dark text-light">
     <div class="container">
         <span>Footer</span>
     </div>
+</footer> -->
+<!-- Footer -->
+<footer class="footer mt-auto text-center text-lg-start text-white" style="background-color: #1c2331">
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">Jonh Smith</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est soluta esse tenetur nesciunt odio saepe minus dolores, ab molestiae officiis pariatur temporibus optio error voluptates sunt eligendi provident excepturi modi.
+                    </p>
+                </div>
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">Destacados</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                    <div class="d-flex flex-column">
+                        <a href="#" class="link-light text-decoration-none mb-3">Lorem ipsum</a>
+                        <a href="#" class="link-light text-decoration-none mb-3">Lorem ipsum</a>
+                        <a href="#" class="link-light text-decoration-none mb-3">Lorem ipsum</a>
+                        <a href="#" class="link-light text-decoration-none">Lorem ipsum</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">Legal</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                    <div class="d-flex flex-column">
+                        <a href="#" class="link-light text-decoration-none mb-3">Lorem ipsum</a>
+                        <a href="#" class="link-light text-decoration-none mb-3">Lorem ipsum</a>
+                        <a href="#" class="link-light text-decoration-none mb-3">Lorem ipsum</a>
+                        <a href="#" class="link-light text-decoration-none">Lorem ipsum</a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <h6 class="text-uppercase fw-bold">Contacto</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                    <p> C/ Tramuntana nº7, Palma, España</p>
+                    <p>info@example.com</p>
+                    <p> + 34 987 654 321</p>
+                    <p> + 34 123 456 789</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © <?= date("Y"); ?> John Smith
+    </div>
 </footer>
+</div>
 </body>
