db64b2584a134d4e395b9099f455ca7e43eaee13
mikelmif <mikel@kydemy.com>
2021-09-27 00:11:45 +0200

    Función para la validación de las imágenes.  Queda pendiente su revisión y modificación para poder ser incluida en el proyecto.

---
+++ services/imageValidationService.php
@@ -0,0 +1,56 @@
+<?php
+function imageValidation ($image) {
+    // Get Image Dimension
+    $fileinfo = getimagesize($image["image"]["tmp_name"]);
+    $width = $fileinfo[0];
+    $height = $fileinfo[1];
+    
+    $allowed_image_extension = array(
+        "png",
+        "jpg",
+        "jpeg",
+        "webp"
+    );
+    
+    // Get image file extension
+    $file_extension = pathinfo($image["file-input"]["name"], PATHINFO_EXTENSION);
+    
+    // Validate file input to check if is not empty
+    if (! file_exists($image["file-input"]["tmp_name"])) {
+        $response = array(
+            "type" => "error",
+            "message" => "Choose image file to upload."
+        );
+    }    // Validate file input to check if is with valid extension
+    else if (! in_array($file_extension, $allowed_image_extension)) {
+        $response = array(
+            "type" => "error",
+            "message" => "Upload valiid images. Only PNG and JPEG are allowed."
+        );
+    }    // Validate image file size
+    else if (($image["file-input"]["size"] > 2000000)) {
+        $response = array(
+            "type" => "error",
+            "message" => "Image size exceeds 2MB"
+        );
+    }    // Validate image file dimension
+    else if ($width > "300" || $height > "200") {
+        $response = array(
+            "type" => "error",
+            "message" => "Image dimension should be within 300X200"
+        );
+    } else {
+        $target = "image/" . basename($image["file-input"]["name"]);
+        if (move_uploaded_file($image["file-input"]["tmp_name"], $target)) {
+            $response = array(
+                "type" => "success",
+                "message" => "Image uploaded successfully."
+            );
+        } else {
+            $response = array(
+                "type" => "error",
+                "message" => "Problem in uploading image files."
+            );
+        }
+    }
+}
